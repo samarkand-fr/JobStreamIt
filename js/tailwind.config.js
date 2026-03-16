04649e665c929495c5d018edf6b2784b61e33fd9
Dina <dina@macbook-pro-de-heba.home>
2026-03-16 11:48:59 +0100

    feat: initial project structure with HTML, CSS, and JS

---
+++ js/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    red: '#FF1E1E',
+                    dark: '#000000',
+                    gray_header: '#708085'
+                }
+            },
+            fontFamily: {
+                oswald: ['Oswald', 'sans-serif'],
+                inter: ['Inter', 'sans-serif'],
+            }
+        }
+    }
+}
